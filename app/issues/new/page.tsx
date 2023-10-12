38ed13e07305c19447619b7e5646643d90b3e95e
Naimur Reza <naimur.rezaa@gmail.com>
2023-10-13 02:33:11 +0600

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
-import { Button, TextField, TextFieldInput } from "@radix-ui/themes";
+import {
+  Button,
+  CalloutRoot,
+  CalloutText,
+  TextField,
+  TextFieldInput,
+} from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 import { Controller, useForm } from "react-hook-form";
 import SimpleMdeReact from "react-simplemde-editor";
@@ -14,27 +21,39 @@ interface IssueForm {
 }
 
 const NewIssue = () => {
+  const [error, setError] = useState("");
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
   const onsubmit = async (data: IssueForm) => {
-    await axios.post("/api/issues", data);
-    router.push("/");
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/");
+    } catch (error) {
+      setError("An unexpected error occurred!");
+    }
   };
   return (
-    <form onSubmit={handleSubmit(onsubmit)} className="max-w-xl p-5 space-y-4">
-      <TextField.Root>
-        <TextFieldInput {...register("title")} placeholder="Title" />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMdeReact placeholder="Description" {...field} />
-        )}
-      />
+    <div className="max-w-xl  p-5 ">
+      {error && (
+        <CalloutRoot color="red" className="mb-5">
+          <CalloutText>{error}</CalloutText>
+        </CalloutRoot>
+      )}
+      <form onSubmit={handleSubmit(onsubmit)} className="space-y-4">
+        <TextField.Root>
+          <TextFieldInput {...register("title")} placeholder="Title" />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMdeReact placeholder="Description" {...field} />
+          )}
+        />
 
-      <Button>Submit the issue</Button>
-    </form>
+        <Button>Submit the issue</Button>
+      </form>
+    </div>
   );
 };
 
