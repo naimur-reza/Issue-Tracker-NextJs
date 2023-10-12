8b1dbfe01c6ddd97157602c9f242c8e690664bed
Naimur Reza <naimur.rezaa@gmail.com>
2023-10-13 01:06:16 +0600

    Build an Api for creating Issue

---
+++ prisma/client.ts
@@ -0,0 +1,19 @@
+// import { PrismaClient } from '@prisma/client'
+
+// const prisma = new PrismaClient()
+
+// async function main() {
+//   await prisma.issue.create({
+//     data: {title: body},
+//   })
+
+// }
+
+// main()
+//   .catch(async (e) => {
+//     console.error(e)
+//     process.exit(1)
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect()
+//   })
