e6c80453eafb04eea921d3d59e47ae834b00b1a9
Naimur Reza <naimur.rezaa@gmail.com>
2023-10-13 19:54:31 +0600

    Add Issue details

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import IssueDetails from "./IssueDetails";
+import { PrismaClient } from "@prisma/client";
+import { notFound } from "next/navigation";
 
-const page = () => {
+interface Props {
+  params: { id: string };
+}
+const prisma = new PrismaClient();
+const page = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: params.id },
+  });
+
+  if (!issue) notFound();
+  console.log(issue);
   return (
     <div>
-      <IssueDetails />
+      Details
+      <IssueDetails issue={issue} />
     </div>
   );
 };
