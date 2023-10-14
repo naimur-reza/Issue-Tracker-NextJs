94c84c4dc56fc965c09bfe405c1c8ba5c7136cc3
Naimur Reza <naimur.rezaa@gmail.com>
2023-10-14 12:46:45 +0600

    Add patch route

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,24 +1,58 @@
-import prisma from "@/prisma/client"
-import { NextRequest, NextResponse } from "next/server"
+import { patchIssueSchema } from "@/app/validationSchema";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  // validated the issue before delete the issue
 
-export async function DELETE(request: NextRequest, {params} : { params: {id : string} }) {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 401 });
 
+  await prisma.issue.delete({
+    where: {
+      id: issue.id,
+    },
+  });
+  return NextResponse.json({});
+}
 
-    // validated the issue before delete the issue
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
 
-    const issue = await prisma.issue.findUnique({
-        where: {
-            id: params.id
-        }
-    })
-    if (!issue) 
-        return NextResponse.json({error: 'Invalid Issue'}, {status: 401})
+  const validation = patchIssueSchema.safeParse(body);
 
-    await prisma.issue.delete({
-        where: {
-            id: issue.id
-        }
-    })
-    return NextResponse.json({})
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  const { title, description } = body;
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 401 });
+
+  // update the issue
+  const updateIssue = await prisma.issue.update({
+    where: {
+      id: params.id,
+    },
+    data: {
+      title,
+      description,
+    },
+  });
+  return NextResponse.json(updateIssue);
 }
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,6 +1,12 @@
 import { z } from 'zod';
 
 export const createdIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    title: z.string().min(1, "Title is required").max(255),
+    description: z.string().min(1, "Description is required").max(6555)
 });
+
+
+export const patchIssueSchema = z.object({
+    title: z.string().min(1, "Title is required").max(255).optional(),
+    description: z.string().min(1, "Description is required").max(6555).optional()
+})
