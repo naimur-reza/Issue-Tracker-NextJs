84d3423abbd0fc2be010f61606468b503f7081a3
Naimur Reza <naimur.rezaa@gmail.com>
2023-10-14 14:14:35 +0600

    Add issue chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
- 
- 
+import prisma from "@/prisma/client";
+import IssueChart from "./IssueChart";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
   return (
     <>
-      Hello
+      <IssueChart inProgress={inProgress} open={open} closed={closed} />
     </>
   );
 };
