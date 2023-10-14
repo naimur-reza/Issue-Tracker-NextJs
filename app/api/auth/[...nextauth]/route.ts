0825245e60d851084dc743a52cf5c2496b4eff17
Naimur Reza <naimur.rezaa@gmail.com>
2023-10-14 21:41:29 +0600

    Add authentication schema

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,4 @@
-import { NextAuthOptions } from "next-auth";
+import { authOptions } from "@/app/auth/authOptions";
 import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-};
 
 export default NextAuth(authOptions);
